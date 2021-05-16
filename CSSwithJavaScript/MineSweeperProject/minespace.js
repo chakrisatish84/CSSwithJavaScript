55fbe0e511e50cd633fcf28d980368f33effcb6a
Sateesh Chava <chakrisatish@gmail.com>
2021-05-16 03:20:29 -0700

    Inserted mine position and win or lose logic.

--- CSSwithJavaScript/MineSweeperProject/minespace.js
+++ CSSwithJavaScript/MineSweeperProject/minespace.js
@@ -1,4 +1,4 @@
-const status = {
+export const status = {
   HIDDEN: "hidden",
   MINE: "mine",
   NUMBER: "number",
@@ -6,6 +6,8 @@ const status = {
 };
 export function createBoard(boardSize, numberofMines) {
   const board = [];
+  const minePositions = createMinePosition(boardSize, numberofMines);
+
   for (var x = 0; x < boardSize; x++) {
     const row = [];
     for (var y = 0; y < boardSize; y++) {
@@ -13,12 +15,105 @@ export function createBoard(boardSize, numberofMines) {
       element.dataset.status = status.HIDDEN;
       const tile = {
         element,
+        mine: minePositions.some((position) =>
+          positionMatch(position, { x, y })
+        ),
         x,
         y,
+        get status() {
+          return this.element.dataset.status;
+        },
+        set status(value) {
+          this.element.dataset.status = value;
+        },
       };
       row.push(tile);
     }
     board.push(row);
   }
   return board;
+}
+
+export function createMinePosition(boardSize, numberofMines) {
+  const positions = [];
+
+  while (positions.length < numberofMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
+    };
+
+    if (!positions.some((p) => positionMatch(p, position))) {
+      positions.push(position);
+    }
+  }
+  return positions;
+}
+
+function positionMatch(a, b) {
+  return a.x == b.x && a.y == b.y;
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
+}
+
+export function markTile(tile) {
+  if (tile.status !== status.HIDDEN && tile.status !== status.MARKED) {
+    return;
+  }
+  if (tile.status === status.HIDDEN) {
+    tile.status = status.MARKED;
+  } else {
+    tile.status = status.HIDDEN;
+  }
+}
+
+export function revealTile(board, tile) {
+  if (tile.status !== status.HIDDEN) {
+    return;
+  }
+
+  if (tile.mine) {
+    tile.status = status.MINE;
+    return;
+  }
+  tile.status = status.NUMBER;
+  const adjacentTiles = nearbyTiles(board, tile);
+  const mineCount = adjacentTiles.filter((tile) => tile.mine).length;
+  if (mineCount == 0) {
+    adjacentTiles.forEach(revealTile.bind(null, board));
+  } else {
+    tile.element.textContent = mineCount;
+  }
+}
+
+function nearbyTiles(board, { x, y }) {
+  const tiles = [];
+  for (let xoffSet = -1; xoffSet <= 1; xoffSet++) {
+    for (let yoffSet = -1; yoffSet <= 1; yoffSet++) {
+      const tile = board[x + xoffSet]?.[y + yoffSet];
+      if (tile) tiles.push(tile);
+    }
+  }
+  return tiles;
+}
+
+export function checkWin(board) {
+  return board.every((row) => {
+    return row.every((tile) => {
+      return (
+        tile.status === status.NUMBER ||
+        (tile.mine &&
+          (tile.status === status.MARKED || tile.status === status.HIDDEN))
+      );
+    });
+  });
+}
+export function checkLose(board) {
+  return board.some((row) => {
+    return row.some((tile) => {
+      return tile.status === status.MINE;
+    });
+  });
 }
--- CSSwithJavaScript/MineSweeperProject/script.js
+++ CSSwithJavaScript/MineSweeperProject/script.js
@@ -5,17 +5,71 @@
 //     a.Mark tiles
 //4) Check for win / Loose.
 
-import { createBoard } from "./minespace.js";
+import {
+  status,
+  createBoard,
+  markTile,
+  revealTile,
+  checkWin,
+  checkLose,
+} from "./minespace.js";
 
 const BOARD_SIZE = 10;
-const NUMBER_OF_TILES = 2;
+const NUMBER_OF_TILES = 1;
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_TILES);
 const boardElement = document.querySelector(".board");
+const minesCountText = document.querySelector("[data-mine-count]");
+const subText = document.querySelector(".subtext");
 
 board.forEach((row) => {
   row.forEach((tile) => {
     boardElement.append(tile.element);
+    tile.element.addEventListener("click", function () {
+      revealTile(board, tile);
+      checkGameEnd();
+    });
+    tile.element.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
+      markTile(tile);
+      updateMinesCount();
+    });
   });
 });
 boardElement.style.setProperty("--size", BOARD_SIZE);
+minesCountText.textContent = NUMBER_OF_TILES;
+
+function updateMinesCount() {
+  const markedMinesCount = board.reduce((count, row) => {
+    return count + row.filter((tile) => tile.status === status.MARKED).length;
+  }, 0);
+  minesCountText.textContent = NUMBER_OF_TILES - markedMinesCount;
+}
+
+function checkGameEnd() {
+  const win = checkWin(board);
+  const lose = checkLose(board);
+
+  if (win || lose) {
+    boardElement.addEventListener("click", stopProp, { capture: true });
+    boardElement.addEventListener("contextmenu", stopProp, { capture: true });
+  }
+  if (win) {
+    subText.textContent = "You Win";
+  }
+  if (lose) {
+    subText.textContent = "You Lose";
+    board.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile.status === status.MARKED) markTile(tile);
+        if (tile.mine) {
+          revealTile(board, tile);
+        }
+      });
+    });
+  }
+}
+
+const stopProp = (e) => {
+  e.stopImmediatePropagation();
+};
